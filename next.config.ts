a872f99c436cbddff12a3a3ad7b1a1754bb11c11
Nick Hageman <nickhageman8@gmail.com>
2026-03-30 18:47:11 -0500

    Modifying next workflow

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // <-- enables static export
 };
 
-export default nextConfig;
+module.exports = nextConfig;
